da8d9a9e2452b9d4cf033631ae400324548d6459
Khaleefa <ktkhaleefakt@gmail.com>
2019-02-27 09:57:25 +0530

    Add a button and set a pop up message while pressing the button

--- flash_card.py
+++ flash_card.py
@@ -2,6 +2,8 @@ import tkinter
 import tkinter as Tk
 from tkinter import messagebox
 
+def pop_up():
+    msg = messagebox.showinfo( "Hello Python", "Hello World")
 
 
     
@@ -12,6 +14,8 @@ window.title("Flash_card game")
 label1=Tk.Label(window, text="welcome to the game", font =("Ariel Bold" ,35))
 label1.grid(column=0, row=0)
 
+btn1 =Tk.Button(window, text ="press", command = pop_up)
+btn1.place(x=100, y=100)
 
 
 window.mainloop()
