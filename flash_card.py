b19c6bab06b9ad1c3e085df852b03d7a0d9de29b
Khaleefa <ktkhaleefakt@gmail.com>
2019-02-27 10:00:47 +0530

    Create function read card for openig the card details line by line(occuring error), that card include my questions and answers

--- flash_card.py
+++ flash_card.py
@@ -5,6 +5,11 @@ from tkinter import messagebox
 def pop_up():
     msg = messagebox.showinfo( "Hello Python", "Hello World")
 
+def open_card():
+    f = open("card.txt","r")
+    f1 = f.readline()
+    for x in f1:
+        print (x)
 
 def close_window (): 
     window.destroy()
@@ -20,6 +25,8 @@ label1.grid(column=0, row=0)
 btn1 =Tk.Button(window, text ="press", command = pop_up)
 btn1.place(x=100, y=100)
 
+btn2 =Tk.Button(window, text ="read card", command = open_card)
+btn2.place(x=200, y=200)
 
 btn3= Tk.Button(window, text="Quit", command=close_window)
 btn3.place(x=250, y =200)
