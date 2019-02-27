697f12b4f366ce18731dc70e2f9534530953da6e
Khaleefa <ktkhaleefakt@gmail.com>
2019-02-27 09:55:54 +0530

    Add label and set the window size

--- flash_card.py
+++ flash_card.py
@@ -1,12 +1,17 @@
 import tkinter
 import tkinter as Tk
+from tkinter import messagebox
 
 
+
+    
 window = tkinter.Tk()
 window.geometry("600x500")
 window.title("Flash_card game")
+
 label1=Tk.Label(window, text="welcome to the game", font =("Ariel Bold" ,35))
 label1.grid(column=0, row=0)
 
-window.mainloop()
 
+
+window.mainloop()
