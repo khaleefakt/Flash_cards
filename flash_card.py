9dd4fb012a9943daf343e1e94f5ebdb7b065f475
Khaleefa <ktkhaleefakt@gmail.com>
2019-03-01 17:26:54 +0530

    add function for correct guess and wrong guess and shows that count in to another label

--- flash_card.py
+++ flash_card.py
@@ -1,7 +1,11 @@
+#import area
 import tkinter
 import tkinter as Tk
 from tkinter import messagebox
-
+#variables
+correct_counter = 0
+wrong_counter =0
+#functions
 def pop_up():
     msg = messagebox.showinfo( "Hello Python", "Hello World")
 
@@ -13,14 +17,27 @@ def read_questions():
             ans = a[1] 
             print("question {} answer {}".format(que,ans))
 
-def close_window (): 
+def close_window ():
     window.destroy()
+
+def correct_guess():
+    global correct_counter
+    correct_counter += 1
+    lab2.config(text= correct_counter)
+
+def wrong_guess():
+    global wrong_counter
+    wrong_counter += 1
+    lab3.config(text= wrong_counter)
+
 
 window = tkinter.Tk()
 window.geometry("500x400")
 window.title("Flash_card game")
 
-
+#question area
+qstnarea =Tk.Label(window,text="",height=8,width =30, bg="white")
+qstnarea.place(x=50, y= 100)
 
 
 label1=Tk.Label(window, text=" welcome to FlashCards", font =("Ariel" ,30))
@@ -29,23 +46,28 @@ label1.grid(column=0, row=0)
 # setting remaining time
 remTime=Tk.Label(window, text="Remaining\n Time", height =2, width = 10)
 remTime.place(x=350, y =50)
-text1 = Tk.Text(window, height=2, width = 10)
-text1.place(x=350, y= 100)
+lab1 = Tk.Label(window, height=2, width = 10,bg="white")
+lab1.place(x=350, y= 100)
 
 #corrcet
-correct=Tk.Label(window, text="corrected", height =3, width = 10)
+correct=Tk.Label(window, text="corrected", height =3, width = 10,bg="red")
 correct.place(x=350, y =120)
-text2 = Tk.Text(window, height=2, width = 10)
-text2.place(x=350, y= 170)
+lab2 = Tk.Label(window, height=2, width = 10,bg="white")
+lab2.place(x=350, y= 170)
+btn1 =Tk.Button(window, text= "I Was Right",command=correct_guess)
+btn1.place(x=50, y=250)
 
 #wrong
 wrong=Tk.Label(window, text="wrong", height =3, width = 10 )
 wrong.place(x=350, y =200)
-text3 = Tk.Text(window, height=2, width = 10)
-text3.place(x=350, y= 250)
+lab3 = Tk.Label(window, height=2, width = 10,bg="white")
+lab3.place(x=350, y= 250)
+btn2 =Tk.Button(window, text= "I Was Wrong",command= wrong_guess)
+btn2.place(x=170, y=250)
 
 #exit Button
 btn3= Tk.Button(window, text="Quit", command=close_window, height=3, width=10)
 btn3.place(x=350, y =310)
+
 
 window.mainloop()
