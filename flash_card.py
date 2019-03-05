7081bb5b7e102fd4615bc3ce70dacd0064bfabee
Khaleefa <ktkhaleefakt@gmail.com>
2019-03-05 17:07:53 +0530

    Modified read_questions()

--- flash_card.py
+++ flash_card.py
@@ -2,14 +2,30 @@
 import tkinter
 import tkinter as Tk
 from tkinter import messagebox
+from tkinter import Frame
+from tkinter import filedialog
+
 #variables
 correct_counter = 0
 wrong_counter =0
+
 #functions
 def pop_up():
     msg = messagebox.showinfo( "Confirmation", "Are You Sure..?")
 
+def myOpen():
+    myOpen = filedialog.askopenfile("/Cards")
+    mlabel4 = Label(myApp,text=myOpen).pack()
+
 def read_questions():
+    cards=[]
+    with open("test_card.txt") as f:
+        for i in f:
+            q,a=i.split()
+            qstnarea.config(text=q)
+            cards.append([q,a])
+    
+"""def read_questions():
     with open("test_card.txt") as f:
         for line in f:
             a = line.split()
@@ -17,7 +33,7 @@ def read_questions():
             ans = a[1]
             qstnarea.config(text=que)
             #print("question {} answer {}".format(que,ans))
-
+"""
 def read_answer():
     with open("test_card.txt") as f:
         for line in f:
@@ -41,17 +57,23 @@ def wrong_guess():
     lab3.config(text= wrong_counter)
 
 
+
+#make the GUI
 window = tkinter.Tk()
-window.geometry("500x400")
+window.geometry("1000x400")
 window.title("Flash_card game")
+
+#make frame
+windowFrame = Frame(window)
+windowFrame.grid(row=5, column=4)
 
 #question area
 qstnarea =Tk.Label(window,text="",height=8,width =30, bg="white")
 qstnarea.place(x=50, y= 100)
 nextqstn=Tk.Button(window, text= "Next Question",command=read_questions, height =3, width =10)
-nextqstn.place(x=60, y=300)
+nextqstn.place(x=50, y=300)
 seeanswr=Tk.Button(window, text= "See Answer",command=read_answer, height =3, width =10)
-seeanswr.place(x=200, y=300)
+seeanswr.place(x=190, y=300)
 
 
 label1=Tk.Label(window, text=" welcome to FlashCards", font =("Ariel" ,30))
@@ -68,7 +90,7 @@ correct=Tk.Label(window, text="corrected", height =3, width = 10,bg="red")
 correct.place(x=350, y =120)
 lab2 = Tk.Label(window, height=2, width = 10,bg="white")
 lab2.place(x=350, y= 170)
-btn1 =Tk.Button(window, text= "I Was Right",command=lambda:[correct_guess(),pop_up()])
+btn1 =Tk.Button(window, text= "I Was Right",command=lambda:[correct_guess(),pop_up()], width=10)
 btn1.place(x=50, y=250)
 
 #wrong
@@ -76,12 +98,44 @@ wrong=Tk.Label(window, text="wrong", height =3, width = 10 )
 wrong.place(x=350, y =200)
 lab3 = Tk.Label(window, height=2, width = 10,bg="white")
 lab3.place(x=350, y= 250)
-btn2 =Tk.Button(window, text= "I Was Wrong",command=lambda:[ wrong_guess(),pop_up()])
-btn2.place(x=170, y=250)
+btn2 =Tk.Button(window, text= "I Was Wrong",command=lambda:[wrong_guess(),pop_up()], width =10)
+btn2.place(x=190, y=250)
 
 #exit Button
 btn3= Tk.Button(window, text="Quit", command=close_window, height=3, width=10)
-btn3.place(x=350, y =310)
+btn3.place(x=350, y =300)
+
+#create the menubar
+menubar =Tk.Menu(window)
+
+#create the file component of that menubar
+filemenu = Tk.Menu(menubar, tearoff=0)
+
+#Add the sub headings to the file menu
+filemenu.add_command(label="Open", command=myOpen)
+menubar.add_cascade(label="File",menu=filemenu)
+
+#add menubar to the window
+window.config(menu=menubar)
+
+
+"""#wrong
+wrong=Tk.Label(windowFrame, text="wrong").grid(row=0, column=0)
+#wrong.place(x=350, y =200)
+lab3 = Tk.Label(windowFrame,bg="white",height=2, width = 10).grid(row=0, column=1)
+#lab3.place(x=350, y= 250)
+btn2 =Tk.Button(windowFrame, text= "I Was Wrong",command=lambda:[ wrong_guess(),pop_up()]).grid(row=0, column=2)
+#btn2.place(x=190, y=250)
+
+#wrong
+wrong=Tk.Label(windowFrame, text="wrong again").grid(row=1, column=0)
+#wrong.place(x=350, y =200)
+lab3 = Tk.Label(windowFrame,bg="white",height=2, width = 10).grid(row=1, column=1)
+#lab3.place(x=350, y= 250)
+btn3 =Tk.Button(windowFrame, text= "I Was Wrong again",command=lambda:[ wrong_guess(),pop_up()]).grid(row=1, column=2)
+#btn2.place(x=190, y=250)
+"""
+
 
 
 window.mainloop()
