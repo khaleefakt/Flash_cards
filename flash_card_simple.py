13c3802f85430a467039d57b86a700cce39caad4
Khaleefa <ktkhaleefakt@gmail.com>
2019-03-11 12:45:29 +0530

    Add restart button, disable load card button after load, right and wrong button shows only after load_card

--- flash_card_simple.py
+++ flash_card_simple.py
@@ -11,6 +11,14 @@ remaining = 60
 def MainFrame():
     def close_window():
         window.destroy()
+    def load_card():
+        Button.loadcard.destroy()
+        cards = []
+        i = f.readline()
+        global a
+        q, a = i.split('-')
+        qstnarea.config(text=q)
+        cards.append([q, a])
     def read_question():
         cards = []
         i = f.readline()
@@ -29,45 +37,59 @@ def MainFrame():
         global wrong_counter
         wrong_counter += 1
         lab3.config(text=wrong_counter)
+    def score():
+        btn1 = Tk.Button(window, name="correct_guess", text="I Was Right",
+                         command=correct_guess, width=10)
+        btn1.place(x=50, y=250)
+        #wrong
+        btn2 = Tk.Button(window, name="wrong_guess", text="I Was Wrong", command=wrong_guess, width=10)
+        btn2.place(x=190, y=250)
+    def restart():
+        MainFrame()
+        window.destroy()
+
     window = Tk.Tk()
     window.geometry("500x400")
     window.title("Flash_card game")
     #question area
     qstnarea = Tk.Label(window, name="qstnarea", text=
-                        """Press Load Card button for
-                        Load the cards, for next question
-                        press Next Question button and
-                        Answer for see the answer""",
+                    """Press Load Card button for
+    Load the cards, for next question
+    press Next Question button and
+    Answer for see the answer""",
                         height=8, width=30, bg="white")
     qstnarea.place(x=50, y=100)
-    loadcard = Tk.Button(window, name="load_card", text="Load Card", command=lambda:[read_question(), rem_time()], height=1, width=10)
+    def disable():
+        loadcard.config(state="disabled")
+    loadcard = Tk.Button(window, name="load_card", text="Load Card",
+                         command=lambda:[read_question(),score(),disable()], height=1, width=10)
     loadcard.place(x=50, y=60)
-    nextqstn = Tk.Button(window, name="next_qstn", text="Next Question", command=read_question, height=3, width=10)
+    nextqstn = Tk.Button(window, name="next_qstn", text="Next Question",
+                         command=lambda:[read_question()], height=3, width=10)
     nextqstn.place(x=50, y=300)
-    seeanswr = Tk.Button(window, name="see_answr", text= "See Answer", command=read_answer, height=3, width=10)
+    seeanswr = Tk.Button(window, name="see_answr", text= "See Answer",
+                         command=read_answer, height=3, width=10)
     seeanswr.place(x=190, y=300)
     # setting remaining time
-    remTime = Tk.Label(window, text="Remaining\n Time", height=2, width=10)
-    remTime.place(x=350, y=60)
-    timeshow = Tk.Label(window, height=1, width=10, bg="white")
-    timeshow.place(x=350, y=100) 
+    # remTime = Tk.Label(window, text="Remaining\n Time", height=2, width=10)
+    # remTime.place(x=350, y=60)
+    # timeshow = Tk.Label(window, height=1, width=10, bg="white")
+    # timeshow.place(x=350, y=100) 
     #corrcet
     correct = Tk.Label(window, text="correct\n Guess", height=2, width=10) #bg="red"
-    correct.place(x=350, y =140)
+    correct.place(x=350, y =100)
     lab2 = Tk.Label(window, height=1, width=10,bg="white")
-    lab2.place(x=350, y=180)
-    btn1 = Tk.Button(window, name="correct_guess", text="I Was Right", command=correct_guess, width=10)
-    btn1.place(x=50, y=250)
-    #wrong
+    lab2.place(x=350, y=140)
     wrong = Tk.Label(window, text="wrong\n Guess", height=2, width=10 )
-    wrong.place(x=350, y=210)
+    wrong.place(x=350, y=170)
     lab3 = Tk.Label(window, height=1, width=10, bg="white")
-    lab3.place(x=350, y=250)
-    btn2 = Tk.Button(window, name="wrong_guess", text="I Was Wrong", command=wrong_guess, width=10)
-    btn2.place(x=190, y=250)
+    lab3.place(x=350, y=210)
+    
     #exit Button
     btn3 = Tk.Button(window, text="Quit", command=close_window, height=3, width=10)
     btn3.place(x=350, y=300)
+    btnclr = Tk.Button(window, text="Restart", command=restart, height=1, width=10)
+    btnclr.place(x=350, y=5)
     return window
 
 def main():
