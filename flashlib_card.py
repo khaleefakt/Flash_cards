449e869cbc118c1f8abdf8319d89b7681081842c
Khaleefa <ktkhaleefakt@gmail.com>
2019-03-25 17:31:53 +0530

    Add Answer key separate , remove frame, not add function for see the answer

--- flashlib_card.py
+++ flashlib_card.py
@@ -1,40 +1,61 @@
 #import flshlib
 import tkinter as Tk
+import random
 
-
-def load_cards():
-    buttonload.destroy()
-    fname = "test_card.txt"
+def load_cards(fname):
     cards = {}
     with open(fname) as f:
         for i in f:
             i = i.strip()
             q, a = i.split(" - ")
             cards[q] = a
-    qstnarea.config(text=cards[q])
+    return cards
+def loadcards(cards, qstnarea):
+    ques = list(cards.keys())
+    qstnarea.config(text=ques[0])
+def destroy_button(buttonload):
+    buttonload.destroy()
+
+def get_answer_for_qn(cards, qn):
+    return cards[qn]
+def get_answer(cards, qn,qstnarea):
+    ans = list(cards.values())
+    qstnarea.config(text=ans[0])
+def ans_frame(window,cards,qn,qstnarea):
+    seeanswr= Tk.Button(window, name="show_answer", text ="Show Answer", command=get_answer(cards,qn,qstnarea), height = 2, width = 30)
+    seeanswr.place(x=20,y=250)
 
 
-window = Tk.Tk()
-window.geometry("300x400")
-window.title("Simple Flash Card")
+    
+def get_next_card(cards):
+    return random.choice(list(cards))
+
+    
+def main():    
+    window = Tk.Tk()
+    window.geometry("300x400")
+    window.title("Simple Flash Card")
 
 
-#cards = load_cards()
-#state = create_scores()
+    cards = load_cards("test_card.txt")
+    # state = create_scores()
+    qn = get_next_card(cards)
 
-qstnarea = Tk.Label(window,  text="", height=8, width=33, bg="white")
-qstnarea.place(x=20,y=60)
-buttonload= Tk.Button(window, name="load_Button", text ="Load Card",command=load_cards)
-buttonload.place(x=20,y=20)
+    qstnarea = Tk.Label(window,  text="", height=8, width=33, bg="white")
+    qstnarea.place(x=20,y=60)
+    buttonload= Tk.Button(window, name="load_Button", text ="Load Card",
+                          command=lambda:[loadcards(cards,qstnarea),
+                                          destroy_button(buttonload),
+                                          ans_frame(window,cards,qn,qstnarea)])
+    buttonload.place(x=20,y=20)
+
+  
+    window.mainloop()
+
+    
+main()
 
 
-
-# f1 = Tk.Frame(name='frame', bg="white", height = 4, width=33)
-# f1.place(x=20, y=250)
-# l = Tk.Label(f1, name="card_label")
-# l.pack()
-
-window.mainloop()
 # def main():
 #     cards = load_cards('test_card.txt')
 #     state = create_scores()
@@ -55,6 +76,40 @@ window.mainloop()
 # if __name__ == "__main__":
 #     main()
 
+
+
+# import random
+
+# def create_scores():
+#     return dict(right = 0,
+#                 wrong = 0)
+
+        
+# def load_cards(fname):
+#     cards = {}
+#     with open(fname) as f:
+#         for i in f:
+#             i = i.strip()
+#             q, a = i.split(" - ")
+#             cards[q] = a
+#     return cards
+
+# def get_next_card(cards):
+#     return random.choice(list(cards))
+
+# def get_answer_for_qn(cards, qn):
+#     return cards[qn]
+
+# def process_right_answer(scores):
+#     scores['right'] += 1
+
+# def process_wrong_answer(scores):
+#     scores['wrong'] += 1
+
+
+
+
+        
 
 
 
